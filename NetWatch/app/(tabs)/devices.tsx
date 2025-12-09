4d59cc1c1da51fdad0b4c99a844f00d5aa29b3fc
Aluno São Lucas <aluno.saolucas@ensino.ulbra.net>
2025-12-09 17:15:39 -0300

    Adicionando todo o ProjetoNetWatch

---
+++ NetWatch/app/(tabs)/devices.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function DeviceList() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Lista de dispositivos aparecerá aqui</Text>
+    </View>
+  );
+}
